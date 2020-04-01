10d1908bcfe5c12a21857ba4c7ec472820636882
nuLL-cmd <programadormoderador@hotmail.com>
2020-03-31 21:17:12 -0300

    Atualização:
    Alteração
     Foi eliminado a consulta de range, e adicionado a trasnferencia
     de lista de uma tela para a outra, tornando a apliacção mais
     eficiente e rapida na transição entre estas actities

--- app/src/main/java/com/automatodev/coinSee/view/activity/ChartActivity.java
+++ app/src/main/java/com/automatodev/coinSee/view/activity/ChartActivity.java
@@ -1,35 +1,29 @@
 package com.automatodev.coinSee.view.activity;
 
-import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NavUtils;
 
 import com.automatodev.coinSee.R;
-import com.automatodev.coinSee.controller.callback.API.AwesomeCallback;
-import com.automatodev.coinSee.controller.entity.CoinChildr;
-import com.automatodev.coinSee.controller.service.API.AwesomeService;
+import com.automatodev.coinSee.controller.entity.CoinRangeEntityAlpha;
 import com.automatodev.coinSee.view.component.ChartLine;
 import com.github.mikephil.charting.charts.LineChart;
 
 import java.util.List;
 
-@RequiresApi(api = Build.VERSION_CODES.P)
+
 public class ChartActivity extends AppCompatActivity {
     public static boolean status;
     private ChartLine chartLine;
     private LineChart lineChart;
     private RelativeLayout relativeChart_chart;
     private ProgressBar progressChart_chart;
-    private AwesomeService awesomeService;
     private TextView txtTitleCoin_chart;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,27 +32,18 @@ public class ChartActivity extends AppCompatActivity {
         relativeChart_chart = findViewById(R.id.relativeChart_chart);
         progressChart_chart = findViewById(R.id.progressChart_chart);
         txtTitleCoin_chart = findViewById(R.id.txtTitleCoin_chart);
-        awesomeService = new AwesomeService(this);
         getData();
     }
 
     public void getData() {
-        Bundle bundle = getIntent().getExtras();
+        final Bundle bundle = getIntent().getExtras();
         if (bundle != null) {
+            List<CoinRangeEntityAlpha> l = (List<CoinRangeEntityAlpha>) bundle.getSerializable("list");
             txtTitleCoin_chart.setText(bundle.getString("dataCoin"));
-            awesomeService.requestRangeDays(bundle.getString("dataChart"), new AwesomeCallback() {
-                @Override
-                public void onSucces(List<CoinChildr> coinChildrList) throws InterruptedException {
-                    chartLine = new ChartLine(ChartActivity.this, lineChart, coinChildrList, 1);
-                    chartLine.makeGraph();
-                    relativeChart_chart.setVisibility(View.GONE);
-                    progressChart_chart.setVisibility(View.GONE);
-                }
-
-                @Override
-                public void onSucces(CoinChildr coinChildr0) throws InterruptedException {
-                }
-            });
+            chartLine = new ChartLine(ChartActivity.this, lineChart, l, bundle.getString("dataCoin"));
+            chartLine.makeGraph();
+            relativeChart_chart.setVisibility(View.GONE);
+            progressChart_chart.setVisibility(View.GONE);
         }
     }
     public void backDetailsActivity(View view){
